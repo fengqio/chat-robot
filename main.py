7a79d0f43e6c6b27d8721f31eadc159a092dd847
chenqi <chenqi@maycur.com>
2023-03-16 17:02:21 +0800

    server port

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from api.ding_server import app
+from config import config
+
+config_info = config.read_sys_config_path()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run(port=8080)
+    app.run(port=config_info.get("server", "port"))
